f44c40aa8706b0b065483d917db5e171d1c8b5dd
ricardo <rgaldom614@g.educaand.es>
2022-03-07 20:51:03 +0100

    boton + redes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,114 +1,21 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
-@Composable
-fun centralButton(){
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.End
-    ) {
-        //Image("C:\\Users\\usuarioT\\Downloads\\a.png")
-        Column { /*...*/ }
-    }
-}
-@Composable
-fun example(){
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
-}
-@Composable
-fun ArtistCard() {
-    Row() {
-        Text("aaaa")
-        Column {
-            Text("Alfred Sisley")
-            Text("3 minutes ago")
-        }
-    }
-}
-
-@Composable
-fun ventana(){
-
-        //En surface pongo las propiedades
-        Surface(
-
-            //color = MaterialTheme.colors.primarySurface,
-            //border = BorderStroke(1.dp, MaterialTheme.colors.secondary),
-            //shape = RoundedCornerShape(8.dp),
-            //elevation = 8.dp
-
-
-        ){
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                //AQUI GRADIENT
-                                MaterialTheme.colors.primary,
-                                MaterialTheme.colors.primaryVariant
-                            )
-                        )
-                    )
-                ,
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = "example",
-                    modifier = Modifier.padding(8.dp)
-                )
-                Row(){
-                    //Text("aaaa")
-                }
-            }
-        }
-
-}
-
-@Composable
-fun ventana2(){
-    Surface(
-        //color = MaterialTheme.colors.primarySurface
-    )
-    {
-
-        Column(
-
-        )
-        {
-            Text("aaa")
-            Row{
-                Text("aa")
-            }
-        }
-    }
+fun launchChrome(link: String){
+    Runtime.getRuntime().exec(arrayOf("cmd", "/c", "start chrome ${link}"))
 }
 
 @Composable
@@ -125,8 +32,26 @@ fun botonContador(){
     }
 }
 @Composable
-fun botonFantasia(){
-
+fun botonTwitter(){
+    var buttonColor = Color(0xFFFFE77A)
+    var twitterBlue = Color(0xFF00ACEE)
+    Button(
+        onClick = { launchChrome("twitter.com") },
+        colors = ButtonDefaults.textButtonColors(backgroundColor = twitterBlue)
+    ) {
+        Text("Twitter", fontSize = 15.sp,fontFamily = FontFamily.Serif,color = Color.White)
+    }
+}
+@Composable
+fun botonInstagram(){
+    var buttonColor = Color(0xFFFFE77A)
+    var instagramPink = Color(0xFFE1306C)
+    Button(
+        onClick = { launchChrome("instagram.com") },
+        colors = ButtonDefaults.textButtonColors(backgroundColor = instagramPink)
+    ) {
+        Text("Instagram", fontSize = 15.sp,fontFamily = FontFamily.Serif,color = Color.White)
+    }
 }
 
 @Composable
@@ -149,7 +74,12 @@ fun paint(){
         )
         {
             botonContador()
-            botonFantasia()
+            Row(
+                Modifier.padding(top = 8.dp)
+            ) {
+            botonTwitter()
+            botonInstagram()
+        }
         }
     }
 }
